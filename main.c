aa1cfb4ac56ca41e524ec256787538c118a4e50e
avichai1221 <57321080+avichai1221@users.noreply.github.com>
2020-01-12 00:16:39 +0200

    Update main.c

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main( int argc ,char *argv[]) {
     char b2[]="all allo aloe an ant are alot ate be allot";
     char b3[]="abc bac bac abc abc ddd aa66a";*/
 
-   if(argc==1)
+   if(argc>1)
     {
 
 fgets(string, 500, stdin);
